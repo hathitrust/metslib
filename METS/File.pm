bc5001fbaa2abc1eb560066fee1bca439ff4a06f
Aaron Elkiss <aelkiss@umich.edu>
2017-12-08 18:09:49 -0500

    METS file element - don't read local file if not necessary

--- METS/File.pm
+++ METS/File.pm
@@ -39,19 +39,22 @@ sub set_local_file {
     $self->compute_md5_checksum()
         if ( not defined $self->{'attrs'}{'CHECKSUM'} );
 
-    my @stat = stat( $self->{'path'} . $self->{'local_file'} )
-        or croak("Cannot stat $self->{'local_file'}");
-    my $size = $stat[7];
-    my $mtime = strftime( "%Y-%m-%dT%H:%M:%SZ", gmtime( $stat[9] ) );
-    $self->{'attrs'}{'SIZE'} = $size if not defined $self->{'attrs'}{'SIZE'};
+    if(not defined $self->{'attrs'}{'SIZE'}
+        or not defined $self->{'attrs'}{'CREATED'}) {
+      my @stat = stat( $self->{'path'} . $self->{'local_file'} )
+          or croak("Cannot stat $self->{'local_file'}");
+      my $size = $stat[7];
+      my $mtime = strftime( "%Y-%m-%dT%H:%M:%SZ", gmtime( $stat[9] ) );
+      $self->{'attrs'}{'SIZE'} = $size if not defined $self->{'attrs'}{'SIZE'};
 
-    # By default use the mtime since there's no reliable way to get the
-    # creation time
-    $self->{'attrs'}{'CREATED'} = $mtime
+      # By default use the mtime since there's no reliable way to get the
+      # creation time
+      $self->{'attrs'}{'CREATED'} = $mtime
         if not defined $self->{'attrs'}{'CREATED'};
+    }
 
     $self->{'attrs'}{'MIMETYPE'} = $self->get_mimetype()
-	if ( not defined $self->{'attrs'}{'MIMETYPE'} );
+      if ( not defined $self->{'attrs'}{'MIMETYPE'} );
 
 }
 
